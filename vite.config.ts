aa9fbd574034a94ba97bee27949399c91ae1d838
sun123zxy <sun123zxy@qq.com>
2026-08-31 21:12:49 +0800

    setup github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+ export default defineConfig({
+    plugins: [react()],
+    base: process.env.GITHUB_ACTIONS
+      ? '/ninegates-webtools/'
+      : '/',
+  })
